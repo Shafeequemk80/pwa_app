b6f27ee95851ebf6181af41325069b39f90b3499
Shafeequemk80 <shafeequemk80@gmail.com>
2024-08-02 17:48:18 +0530

    notification added

--- public/sw.js
+++ public/sw.js
@@ -1,30 +1,42 @@
 let cacheData = "appV1";
+this.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(cacheData).then((cache) => {
+            cache.addAll([
+                '/static/js/main.chunk.js',
+                '/static/js/0.chunk.js',
+                '/static/js/bundle.js',
+                '/static/css/main.chunk.css',
+                '/bootstrap.min.css',
+                '/index.html',
+                '/',
+                "/users"
+            ])
+        })
+    )
+})
+this.addEventListener("fetch", (event) => {
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(cacheData).then((cache) => {
-      return cache.addAll([
-        "/static/js/bundle.js",
-        "/static/js/main.chunk.js",
-        "/static/js/0.chunk.js",
-        "/index.html",
-        "/",
-        "/users",
-      ]);
-    })
-  );
-});
 
-self.addEventListener("fetch", (event) => {
-  if (!navigator.onLine) {
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        if(response){
-            return response 
+    // console.warn("url",event.request.url)
+
+
+    if (!navigator.onLine) {
+        if (event.request.url === "http://localhost:3000/static/js/main.chunk.js") {
+            event.waitUntil(
+                this.registration.showNotification("Internet", {
+                    body: "internet not working",
+                })
+            )
         }
-        let fetchUrl= event.request.clone()
-        fetch(fetchUrl)
-      })
-    );
-  }
-});
+        event.respondWith(
+            caches.match(event.request).then((resp) => {
+                if (resp) {
+                    return resp
+                }
+                let requestUrl = event.request.clone();
+                fetch(requestUrl)
+            })
+        )
+    }
+}) 
